e503406806f6c2b28abe7d6f2c6e723a2c526690
EronixMC <107801782+EronixMC@users.noreply.github.com>
2022-12-07 21:55:16 +0100

    Create renderClient.js

---
+++ renderClient.js
@@ -0,0 +1,28 @@
+const { Client, Authenticator } = require('minecraft-launcher-core');
+const launcher = new Client();
+const token = "24515635345EXC152456";
+
+let opts = {
+    clientPackage: null,
+    authorization: Authenticator.getAuth("Fabien_Ferrari", token.value),
+    root: getAppDataPath(".eronix"),
+    version: {
+        number: "1.12.2",
+        type: "release",
+    },
+    memory: {
+        max: `4G`,
+        min: "1G"
+    },
+    server: {
+        host: "localhost",
+        port: "25565"
+    }
+}
+
+function play() {
+    launcher.launch(opts);
+}
+
+launcher.on('debug', (e) => console.log(e));
+launcher.on('data', (e) => console.log(e));
